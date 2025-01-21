mongodb-forks/spidermonkey

Simplify calls to mozjs StringBuffer::append(const char (&array)[ArrayLength])
In Clang/g++, type deduction figures out that both sides of the
ternary operator (see the diff) can be a char[8] array, which allows
overload resolution to choose the
StringBuffer::append(const char (&array)[ArrayLength]) overload, where
ArrayLength is a template parameter with value 8.

MSVC's type deduction cannot make this inference, however, so we need
to interchange the outer StringBuffer::append() call with the inner
ternary operator, simplifying inference. The resulting code is
arguably more robust: before this change, the code was only legal
because the ".getter" and ".setter" strings happen to have the same
length.

js/src/frontend/Parser.cpp
@@ -7731,8 +7731,9 @@ bool GeneralParser<ParseHandler, Unit>::classMember(
         if (!storedMethodName.append(this->parserAtoms(), propAtom)) {
           return false;
         }
-        if (!storedMethodName.append(
-                atype == AccessorType::Getter ? ".getter" : ".setter")) {
+        if (!((atype == AccessorType::Getter)
+                  ? storedMethodName.append(".getter")
+                  : storedMethodName.append(".setter"))) {
           return false;
         }
         auto storedMethodProp =

mongodb-forks/gecko-dev

js/src/frontend/Parser.cpp
@@ -9040,8 +9040,10 @@ GeneralParser<ParseHandler, Unit>::synthesizePrivateMethodInitializer(
   if (!storedMethodName.append(this->parserAtoms(), propAtom)) {
     return null();
   }
-  if (!storedMethodName.append(
-          accessorType == AccessorType::Getter ? ".getter" : ".setter")) {
+
+  if (!((accessorType == AccessorType::Getter)
+            ? storedMethodName.append(".getter")
+            : storedMethodName.append(".setter"))) {
     return null();
   }
   auto storedMethodProp =